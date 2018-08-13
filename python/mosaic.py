ce4e7616176de9125aeecf2f4a92b2c963660021
David Smith <david.smith@gmail.com>
2018-08-13 13:51:12 -0500

    add padding option to mosaic

--- python/mosaic.py
+++ python/mosaic.py
@@ -1,4 +1,4 @@
-def mosaic(img):
+def mosaic(img, pad=True):
     """
     Create a 2-D mosaic of images from an n-D image. An attempt is made to
     make the resulting 2-D image as square as possible.
@@ -6,7 +6,8 @@ def mosaic(img):
     Parameters
     ----------
     img : ndarray
-        n-dimensional image be tiled into a mosaic
+        n-dimensional image be tiled into a mosaic. All but last two dims are
+        lumped.
 
     Returns
     -------
@@ -14,28 +15,43 @@ def mosaic(img):
         Tiled mosaic of images.
     """
     from numpy import ix_, array, zeros, arange
-    from math import sqrt, floor
+    from math import sqrt, floor, ceil
     if len(img.shape) <= 2:   # already 2-D, so skip the rest
         return img
     img = array(img)
     nr, nc = img.shape[-2:]    # take off last two dimensions, rest are lumped.
+    print('img.shape=',img.shape,end=None)
     img = img.reshape((-1, nr, nc))
+    print(' -> ', img.shape)
     nz = img.shape[0]
-    n = int(floor(sqrt(nz)))  # starting guess for tiling dimensions
-    # find largest integer less than or equal to sqrt that evenly divides the
-    # number of 2-d images in the n-d image.
-    m = [x for x in range(1, n+1) if nz % x == 0]
-    m = m[-1]
-    j = nz // m      # figure out the most square dimensions
-    n2 = min(j, m)
-    n1 = max(j, m)
-    M = zeros((nr*n2, nc*n1), dtype=img.dtype)
-    for j2 in range(n2):      # stick them together
-        for j1 in range(n1):  # there is probably a better way to do this
-            rows = nr*j2 + arange(nr)
-            cols = nc*j1 + arange(nc)
-            M[ix_(rows, cols)] = img[j1+j2*n1, :, :]
-    return M
+
+    if pad:
+        n = int(ceil(sqrt(nz)))  # starting guess for tiling dimensions
+        M = zeros((nr*n, nc*n), dtype=img.dtype)
+        for j2 in range(n):      # stick them together
+            for j1 in range(n):  # there is probably a better way to do this
+                if j1+j2*n >= nz:
+                    break
+                rows = nr*j2 + arange(nr)
+                cols = nc*j1 + arange(nc)
+                M[ix_(rows, cols)] = img[j1+j2*n, :, :]
+        return M
+    else:
+        n = int(floor(sqrt(nz)))  # starting guess for tiling dimensions
+        # find largest integer less than or equal to sqrt that evenly divides the
+        # number of 2-d images in the n-d image.
+        m = [x for x in range(1, n+1) if nz % x == 0]
+        m = m[-1]
+        j = nz // m      # figure out the most square dimensions
+        n2 = min(j, m)
+        n1 = max(j, m)
+        M = zeros((nr*n2, nc*n1), dtype=img.dtype)
+        for j2 in range(n2):      # stick them together
+            for j1 in range(n1):  # there is probably a better way to do this
+                rows = nr*j2 + arange(nr)
+                cols = nc*j1 + arange(nc)
+                M[ix_(rows, cols)] = img[j1+j2*n1, :, :]
+        return M
 
 
 def main():
