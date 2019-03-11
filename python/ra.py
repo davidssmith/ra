074498c53989d8e604144b7078b491e3f9eedd19
David Smith <david.smith@gmail.com>
2019-03-11 18:09:56 -0500

    remove compression

--- python/ra.py
+++ python/ra.py
@@ -25,7 +25,8 @@
 import struct
 import numpy as np
 
-FLAG_BIG_ENDIAN = 0x01
+FLAG_BIG_ENDIAN = 0b1
+FLAG_COMPRESSED = 0b10
 MAGIC_NUMBER = 8746397786917265778
 dtype_kind_to_enum = {'i':1,'u':2,'f':3,'c':4}
 dtype_enum_to_name = {0:'user',1:'int',2:'uint',3:'float',4:'complex'}
@@ -34,50 +35,50 @@ def read(filename):
     f = open(filename,'rb')
     h = getheader(f)
     h['dims'] = h['dims'][::-1]
+    data = f.read()
     if h['eltype'] == 0:
         print('Unable to convert user data. Returning raw byte string.')
-        data = f.read(h['size'])
     else:
         d = '%s%d' % (dtype_enum_to_name[h['eltype']], h['elbyte']*8)
-        data = np.fromstring(f.read(h['size']), dtype=np.dtype(d))
+        data = np.fromstring(data, dtype=np.dtype(d))
         data = data.reshape(h['dims']).transpose()
     f.close()
     return data
 
 
 def getheader(f):
-    filemagic = f.read(8)
+    buf = f.read(48)
+    header_data = np.frombuffer(buf, '<Q')
     h = dict()
-    h['flags'] = struct.unpack('<Q',f.read(8))[0]
-    h['eltype'] = struct.unpack('<Q',f.read(8))[0]
-    h['elbyte'] = struct.unpack('<Q',f.read(8))[0]
-    h['size'] = struct.unpack('<Q',f.read(8))[0]
-    h['ndims'] = struct.unpack('<Q',f.read(8))[0]
-    h['dims'] = []
-    for d in range(h['ndims']):
-        h['dims'].append(struct.unpack('<Q',f.read(8))[0])
+    h['flags'] = header_data[1]
+    h['eltype'] = header_data[2]
+    h['elbyte'] = header_data[3]
+    h['size'] = header_data[4]
+    h['ndims'] = header_data[5]
+    buf = f.read(int(8*h['ndims']))
+    h['dims'] = np.frombuffer(buf, '<Q')
     return h
 
 
 def query(filename):
-  q = "---\nname: %s\n" % filename
-  fd = open(filename,'r')
-  h = getheader(fd)
-  fd.close()
-  if h['flags'] & FLAG_BIG_ENDIAN:
-    endian = 'big'
-  else:
-    endian = 'little'
-  assert endian == 'little'  # big not implemented yet
-  q += 'endian: %s\n' % endian
-  q += 'type: %s%d\n' % (dtype_enum_to_name[h['eltype']], h['elbyte']*8)
-  q += 'size: %d\n' % h['size']
-  q += 'dimension: %d\n' % h['ndims']
-  q += 'shape:\n'
-  for d in h['dims']:
-     q += '  - %d\n' % d
-  q += '...'
-  return q
+    q = "---\nname: %s\n" % filename
+    fd = open(filename,'r')
+    h = getheader(fd)
+    fd.close()
+    if h['flags'] & FLAG_BIG_ENDIAN != 0:
+        endian = 'big'
+    else:
+        endian = 'little'
+    assert endian == 'little'  # big not implemented yet
+    q += 'endian: %s\n' % endian
+    q += 'type: %s%d\n' % (dtype_enum_to_name[h['eltype']], h['elbyte']*8)
+    q += 'size: %d\n' % h['size']
+    q += 'dimension: %d\n' % h['ndims']
+    q += 'shape:\n'
+    for d in h['dims']:
+        q += '  - %d\n' % d
+    q += '...'
+    return q
 
 
 def write(data, filename):
